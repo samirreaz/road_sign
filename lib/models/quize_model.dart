a5d25c92cb2a8b55e42f9a80b6721d070cb13e0b
Mohammadshahsamirbhu <samir_reaz@yahoo.com>
2022-01-31 05:42:52 +0600

    samir change

---
+++ lib/models/quize_model.dart
@@ -0,0 +1,13 @@
+class Quize {
+  final String question;
+
+  Quize({required this.question});
+}
+
+List<Quize> quizeList = [
+  Quize(question: 'what is Red light?'),
+  Quize(question: 'what is Green light?'),
+  Quize(question: 'what is side walk?'),
+  Quize(question: 'what is zebra crossing?'),
+  Quize(question: 'what are the traffic lights?'),
+];
