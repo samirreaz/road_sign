b25dd3126c9e787dd5759ea5e30a51f08828d882
abirhasnat95 <abirhasnat@2016@gmail.com>
2022-02-17 13:25:39 +0600

    Almost Done

--- lib/screen/extra_screen/about.dart
+++ lib/screen/extra_screen/about.dart
@@ -2,7 +2,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:road_sign/screen/home/home_screen.dart';
 import 'package:url_launcher/url_launcher.dart';
-import '';
 
 var defultText = TextStyle(color: Colors.black, fontSize: 20);
 var linkText = TextStyle(color: Colors.blue, fontSize: 20);
@@ -104,65 +103,3 @@ class About_App extends StatelessWidget {
     ));
   }
 }
-
-
-//!SingleChildScrollView was Applied here:-
-        // Center(
-        //     child: SingleChildScrollView(
-        //         child: Padding(
-        //   padding: const EdgeInsets.all(80.0),
-        //   child: Center(
-        //     child: Text('Welcome to Road Sign' + '\n Made by Abir Hasnat' + 'Contact Informations ►' + '\n Github Link: https://github.com/abirhasnat95' + '\n LinkedIn: www.linkedin.com/in/abirhasnat95/',
-        //     style: TextStyle(fontSize: 24),)
-        //   ),
-        // ),),),
-//!SingleChildScrollView was Applied here:
-
-//!Previous About
-
-      // appBar: PreferredSize(
-      //   preferredSize: Size.fromHeight(300),
-      //   child: AppBar(
-      //     title: Text(
-      //       'About this App',
-      //       style: TextStyle(fontSize: 38),
-      //     ),
-      //     centerTitle: true,
-      //     backgroundColor: Color(0xffbf360c),
-      //     elevation: 20,
-      //     shadowColor: Color(0xff3f51b5),
-      //     shape: RoundedRectangleBorder(
-      //       borderRadius: BorderRadius.only(
-      //         bottomLeft: Radius.circular(25),
-      //         bottomRight: Radius.circular(25),
-      //       ),
-      //     ),
-      //   ),
-      // ),
-      // backgroundColor: Color(0xffffcdd2),
-      // body: Column(
-      //   mainAxisAlignment: MainAxisAlignment.start,
-      //   children: [
-      //     Container(
-      //       child: CircleAvatar(
-      //         maxRadius: 150,
-      //         backgroundImage: NetworkImage(
-      //             'https://media-exp1.licdn.com/dms/image/C5603AQEzw6Dk3IPgHA/profile-displayphoto-shrink_800_800/0/1608714667178?e=1650499200&v=beta&t=LOwP7T4YXz7as8iYcmzjrtdBcAVMBUN5hGSkSgSDl-8'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 30,
-      //     ),
-      //     Column(
-      //       children: [
-      //         Row(
-      //           mainAxisAlignment: MainAxisAlignment.center,
-      //           children: [
-      //             Text('Abir Hasnat'),
-      //           ],
-      //         ),
-      //       ],
-      //     )
-      //   ],
-      // ),
-      //! previous about
